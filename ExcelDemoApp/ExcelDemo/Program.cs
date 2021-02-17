07b70b6ac3ed9972f02c95df97f17e6dc3dad8d0
Surajbeone <beprofessioalwithme@gmail.com>
2021-02-17 20:38:26 +0530

    First Commit

---
+++ ExcelDemoApp/ExcelDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExcelDemo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ReadDataFromExcel readData = new ReadDataFromExcel("ProductList.xlsx");
+            Console.WriteLine(readData.ReadExcel());
+        }
+
+    }
+}
+
+
+
+
+
